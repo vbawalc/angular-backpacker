6a86ea17b1913f6b76e6ff5a9c511df088bc969d
Bartlomiej Walczak <vbawalc@gmail.com>
2023-08-09 22:30:20 +0200

    fixed weights

--- wardrobe/src/main/java/com/example/wardrobe/itemService.java
+++ wardrobe/src/main/java/com/example/wardrobe/itemService.java
@@ -15,13 +15,14 @@ public class itemService {
     public itemService() {
         itemList = new ArrayList<>();
 
-        Item item1 = new Item(1,"White Tshirt", 32, "tshirt");
-        Item item2 = new Item(2,"Trousers", 26, "trousers");
-        Item item3 = new Item(3,"Jacket", 45, "outwear");
-        Item item4 = new Item(4,"Black Tshirt", 32, "tshirt");
-        Item item5 = new Item(5,"Socks", 59, "underwear");
+        Item item1 = new Item(1,"White Tshirt", 0.4, "tshirt");
+        Item item2 = new Item(2,"Trousers", 1, "trousers");
+        Item item3 = new Item(3,"Jacket", 1.2, "outwear");
+        Item item4 = new Item(4,"Black Tshirt", 0.4, "tshirt");
+        Item item5 = new Item(5,"Socks", 0.1, "underwear");
+        Item item6 = new Item(6,"Underwear", 0.1, "underwear");
 
-        itemList.addAll(Arrays.asList(item1, item2, item3, item4, item5));
+        itemList.addAll(Arrays.asList(item1, item2, item3, item4, item5, item6));
     }
 
     public Optional<Item> getItem(Integer id) {
